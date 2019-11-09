7c38eb91c3547ece108258ab16a294df0a6885e2
Morgane Broutet <morgane.broutet@gmail.com>
2019-11-09 08:25:16 +0100

    add win method

--- js/game.js
+++ js/game.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
             this.player_two = 2;
             this.turn = this.player_one;
             this.max_turn = this.grid_dimension.x * this.grid_dimension.y;
-            this.count_turn = 0;
+            this.count_turn = 0; // nombre de coups joués
             this.winner = null;
             this.grid = [];
     
@@ -26,7 +26,7 @@ $(document).ready(function(){
                     this.grid[h][l] = 0;
                 }
             }
-            console.log(this.grid);
+            console.log("grid",this.grid);
         }
 
         // cette fonction va nous permettre de déterminer ce qui est déclenché au clic
@@ -43,8 +43,18 @@ $(document).ready(function(){
             console.log(this.grid);
 
             this.add_token(y, x);
+
+            console.log("player", this.turn)
+
+            console.log(this.win(y, x, this.turn === 1 ? 2 : 1 ));
+            // on a mis ce this.turn pour obtenir le player qui vient de jouer (car le add_token l'a déjà modifié)          
+           
+            if (this.winner){
+                console.warn("le joueur " + this.winner + " a gagné");
+            }
         }
 
+        // cette fonction va nous permettre d'aller chercher la dernière case vide dans la colonne cliquée
         check_last_index(x){
             for (let h= this.grid_dimension.y - 1; h >= 0; h--){
                     if(this.grid[h][x] === 0){
@@ -53,37 +63,101 @@ $(document).ready(function(){
                 }
         }
 
+        //cette fonction va nous permettre d'ajouter le jeton 
         add_token(y, x){
             // alterner joueur courant (this.turn) (condition)
             // cibler/placer les jetons sur img html (mettre condition pour changer le jeton en fonction du joueur)
             // compter les tours (this.count.turn++)
             // mettre à jour notre grille en mettant 1 ou 2 en fonction du joueur this.grid[y][x]
 
+            
             // attention à bien mettre === dans la condition pour que ce ne soit pas considéré comme une asignation
-
             if (this.turn === this.player_one){
-                $("tr:nth-child(" + (y+1) + ") td:nth-child(" + (x+1) + ") img").attr("src", "./img/jetonswag.png");
+                $("tr:nth-child(" + (y+1) + ") td:nth-child(" + (x+1) + ") img").attr("src", "./img/pacicon.png").attr("class", "jeton");
                 this.turn = this.player_two;
                 this.count_turn ++;
                 } else {
-                    $("tr:nth-child(" + (y+1) + ") td:nth-child(" + (x+1) + ") img").attr("src", "./img/jetonalien.png");
+                    $("tr:nth-child(" + (y+1) + ") td:nth-child(" + (x+1) + ") img").attr("src", "./img/ghosticon.png").attr("class", "jeton");
                     this.turn = this.player_one;
                     this.count_turn ++;
                     }
+
+                    console.log("compteur:", this.count_turn);
+        }
+
+        win(y, x, player){
+            console.log("y", y)
+            console.log("x", x)
+            console.log("player", player)
+
+        // Horizontal
+            let count = 0;
+            for (let l = 0; l < this.grid_dimension.x; l++) {
+                count = (this.grid[y][l] == player) ? count+1 : 0;
+                console.log("count hori", count)
+                if (count >= 4) {
+                    this.winner = player;
+                    return true;
+                }
+            }
+
+        // Vertical
+            count = 0;
+            for (let h = 0; h < this.grid_dimension.y; h++) {
+                count = (this.grid[h][x] == player) ? count+1 : 0;
+                console.log("count verti", count)
+                if (count >= 4) {
+                    this.winner = player;
+                    return true;
+                }
+            }
+
+        // Diagonal  
+            count = 0;
+            // let shift = row - column;
+            let shift = y - x;
+            for (let i = Math.max(shift, 0); i < Math.min(this.grid_dimension.y, this.grid_dimension.x + shift); i++) {
+            count = (this.grid[i][i - shift] == player) ? count+1 : 0;
+                console.log("count diago", count)
+                if (count >= 4) {
+                    this.winner = player;
+                    return true;
+                }
+            }
+        
+        // Anti - diagonal
+            count = 0;
+            // shift = row + column;
+            let second_shift = y + x;
+            for (let i = Math.max(second_shift - this.grid_dimension.x + 1, 0); i < Math.min(this.grid_dimension.y, second_shift + 1); i++) {
+            console.log(i,second_shift-i, second_shift)
+            count = (this.grid[i][second_shift - i] == player) ? count+1 : 0;
+                console.log("count anti diago", count)
+                if (count >= 4){
+                    this.winner = player;
+                    return true;
+                }
+            }
+
             
-                    console.log("compteur:" + this.count_turn);
-
-
 
         }
 
-        // win(){
+        
+    
+        // reset() {
+        //     for (let l = 0; l < this.grid_dimension.x; l++) {
+        //       for (let h = 0; h < this.grid_dimension.y; h++) {
+        //         this.board[i][j] = 0;
+        //       }
+        //     }
+
+
+    
 
         // }
 
-        // reset(){
-            
-        }
+    }
     var game1 = new Puissance4()
     console.log(game1);
     
--- js/menu.js
+++ js/menu.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
+    // function loader() {
+
+    //     var loader = document.querySelector("#loader");
+    //     loader.classList.add("fadeOut");
+    
+    //     setTimeout(function() {
+    //         var fullContent = document.querySelector("#fullContent");
+    //         fullContent.style.display = "initial";
+    //         loader.style.display = "none";
+    //     }, 1000);
+    // }
+
+    setTimeout(function(){
+    $("#loadingContainer").fadeOut("slow");
+    }, 4850);
+
+
     $("#startButton").on("click", function(){
         window.location.href = "game.html";
     })
